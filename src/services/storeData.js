07b0a0beef1caa6481357e57b9b19739ca930383
Muhammad Jauza Zachri <jauza2001@gmail.com>
2024-05-23 03:32:53 +0700

    Push first code, lesgoo!

---
+++ src/services/storeData.js
@@ -0,0 +1,11 @@
+const { Firestore } = require("@google-cloud/firestore");
+
+async function storeData(id, data) {
+  const db = new Firestore();
+
+  db.settings({ ignoreUndefinedProperties: true });
+  const predictCollection = db.collection("prediction");
+  return predictCollection.doc(id).set(data);
+}
+
+module.exports = storeData;
